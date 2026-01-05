c13408e6130bbf23e0ee8fe4634d084796092188
hochkofler94 <hochkofler94@gmail.com>
2026-01-05 09:40:23 -0400

    add worksheet printview

---
+++ src/hoch/lims/browser/printview.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from bika.lims.browser.worksheet.views.printview import PrintView as BasePrintView
+from hoch.lims.utils import get_formatted_specs, get_formatted_interim
+from hoch.lims import logger
+from bika.lims import api
+class PrintView(BasePrintView):
+    """Extends Senaite PrintView without rewriting everything."""
+
+    def get_ws_status(self):
+        ws = None
+        if self._current_ws_index < len(self._worksheets):
+            ws = self._worksheets[self._current_ws_index]
+        
+        return api.get_workflow_status_of(ws) if ws else "unknown"
+    
+    def get_formatted_specs(self, analysis, with_shoulder_range=False,
+                            lt_operator=None, leq_operator=None, 
+                            gt_operator=None, geq_operator=None):
+        """Get formatted specifications for an analysis using custom function."""
+        return get_formatted_specs(analysis, with_shoulder_range=False,
+                            lt_operator=None, leq_operator=None, 
+                            gt_operator=None, geq_operator=None)
+        
+    def get_consumables(self, analysis):
+        consumables_dict = []
+        consumables_raw = analysis.getConsumablesFields()
+        if not consumables_raw:
+            return consumables_dict
+        for consumable in consumables_raw:
+            logger.info("consumable: '%s'", consumable)
+            ref_definition_uid = consumable.get("keyword", '')
+            consumable_uid = consumable.get("value", '')
+            if not ref_definition_uid:
+                continue
+            ref_definition_obj = api.get_object_by_uid(ref_definition_uid)
+            if not ref_definition_obj:
+                continue
+            consumable_obj = api.get_object_by_uid(consumable_uid) or ''
+            consumables_dict.append(
+                {
+                    "ref_definition_obj": ref_definition_obj,
+                    "consumable_obj": consumable_obj,
+                }
+            )
+        return consumables_dict
+    
+    def get_formatted_interim(self, interim):
+        return get_formatted_interim(interim)
